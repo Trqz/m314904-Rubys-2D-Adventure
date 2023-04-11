a3e78fed425485a7e5ec8856941b79087f8de740
Trqz <joaomarcostroquez@gmail.com>
2023-04-12 00:45:43 +0100

    m314904@ADDED health collectibles

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    //[SerializeField]
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    private int currentHealth;
+
+    [SerializeField]
+    private float speed = 3f;
+
     private Rigidbody2D _rigidbody2D;
     private float horizontal, vertical;
 
@@ -11,6 +20,9 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        //currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -23,7 +35,13 @@ public class RubyController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = _rigidbody2D.position;
-        position += new Vector2(horizontal, vertical) * 3f * Time.deltaTime;
+        position += new Vector2(horizontal, vertical) * speed * Time.deltaTime;
         _rigidbody2D.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
