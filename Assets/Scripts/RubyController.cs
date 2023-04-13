46c4db3a36f28b6335c4b0247d9b41d83b8ea734
Trqz <joaomarcostroquez@gmail.com>
2023-04-13 01:34:21 +0100

    m314904@ADDED other animations

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -21,12 +21,17 @@ public class RubyController : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private float horizontal, vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,6 +39,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -57,6 +74,7 @@ public class RubyController : MonoBehaviour
             if (isInvincible)
                 return;
 
+            animator.SetTrigger("Hit");
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
